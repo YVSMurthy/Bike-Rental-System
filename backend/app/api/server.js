0d8e1d3948ab715246f92209ecf9e1af9a14d0fa
Y V Suyash Murthy <suyash.murthy@gmail.com>
2025-11-08 10:40:24 +0530

    project initialisation

---
+++ backend/app/api/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).send({"message": "Server is running"});
+})
+
+app.listen(PORT);
